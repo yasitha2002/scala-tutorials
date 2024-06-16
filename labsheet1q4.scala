591a6cf3f7fb8ee5a62ea1e31761f5880857a69c
yasitha2002 <135542984+yasitha2002@users.noreply.github.com>
2024-06-16 21:33:58 +0530

    Update labsheet1q4.scala

--- labsheet1q4.scala
+++ labsheet1q4.scala
@@ -2,6 +2,8 @@ package Demo
 
 object labsheet1q4 {
  def main(args:Array[String])={
-   
+   def coverprice_cost(n:Int):Double=24.95*0.6*n ;
+  def total_cost(num:Int,cpc:(Int)=>Double):Double = if(num<=50)num*3+cpc(num) else 3*50+(num-50)*0.75+cpc(num) ;
+  println(total_cost(60,coverprice_cost))
  }
 }
